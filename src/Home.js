393ceab6dafdf8ad36f0226d692219e7d4d020c4
Diego Pereira <dhiego_ti@hotmail.com>
2019-08-19 22:32:51 -0300

    Em progresso

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,84 @@
 import React, { Component } from 'react'
-import DatePicker from "react-datepicker";
-import dateFormat from 'dateformat';
-
-import "react-datepicker/dist/react-datepicker.css";
-// CSS Modules, react-datepicker-cssmodules.css
-// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import dateFormat from 'dateformat'
 
 import api from './Api'
-import Menu from './Menu'
 
 class Home extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            isLoading: false,
-            startDate: new Date()
-        }  
-        this.handleChange = this.handleChange.bind(this);      
-    }
-    handleChange(date) {
-        //console.log(date)
-        console.log(dateFormat(date, 'dd/mm/yyyy'))
+            isLoading: false
+        }   
+        this.menu = this.menu.bind(this)
+        this.listPoint = this.listPoint.bind(this)
+    }   
+    getPonto(){
+        //api.loginUser(user).then((res) => {})
+        console.log('verificando...', this.refs.mat.value, dateFormat(this.refs.date.value, 'dd/mm/yyyy'))
+        
         this.setState({
-          startDate: date
-        });
+            isLoading: true
+        })
       }
-    componentDidMount(){
-        //api.loginUser(user).then((res) => {})            
-    }  
+    menu(){
+        return(
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+              <a className="navbar-brand" href="#"><img src='images/logo.png' width='200' /></a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+            
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">Ponto<span className="sr-only">(current)</span></a>
+                  </li>
+                </ul>
+                <div className="form-inline my-2 my-lg-0 form-group" >
+                  <label>Matrícula:</label>
+                  <input ref='mat' className="form-control mr-sm-2" type="search" placeholder="Matrícula" aria-label="Search"/>
+                  <label>Data:</label>
+                  <input ref='date' className="form-control mr-sm-2" type="date" placeholder="Search" aria-label="Data" />
+                  <button className="btn btn-outline-success my-2 my-sm-0" onClick={() => this.getPonto()}>Verificar</button>
+                </div>
+              </div>
+            </nav>
+        )
+    }
+    listPoint(){
+        return(
+            <div className='row row-content'>                    
+                    <div className="card  col-md-8 offset-md-2">
+                        <div className="card-header bg-transparent">Batida 1</div>
+                        <div className="card-body">
+                            <div className="alert alert-primary" role="alert">
+                                <p className='font-weight-bold'>Entrada: 07:45  </p>
+                                <p className='font-weight-bold'>Saída: 12:00 </p>
+                                <p className='font-weight-bold'>Permanência: 03:15</p>
+                            </div>
+                        </div>
+                        <div className="card-header bg-transparent">Batida 2</div>
+                        <div className="card-body">
+                            <div className="alert alert-primary" role="alert">
+                                <p className='font-weight-bold'>Entrada: 07:45  </p>
+                                <p className='font-weight-bold'>Saída: 12:00 </p>
+                                <p className='font-weight-bold'>Permanência: 03:15</p>
+                            </div>
+                        </div>
+                        <div className="card-footer bg-transparent font-weight-bold">Horas trabalhadas: 09:27</div>
+                    </div>
+                </div>
+        )
+    }
     
     render() {
         return(
-            <>
-            <Menu/> 
-            <section id="intro" className="intro-section"> 
-            <DatePicker selected={this.state.startDate} onChange={this.handleChange} />
-            </section>
+            <>                
+                {this.menu()}               
+                {   this.state.isLoading &&
+                    this.listPoint()
+                }
             </>
         )
     }
--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link, Redirect } from 'react-router-dom'
 
 
 class Menu extends Component {
@@ -6,20 +7,59 @@ class Menu extends Component {
         super(props)
 
         this.state = {
+            isLoading: false,
             redirect: false
         }
     }    
+    componentDidMount(){
+      localStorage.setItem('ponto', 0)
+    }
+    getPonto(){
+      //api.loginUser(user).then((res) => {})
+      console.log('verificando...') 
+      this.setState({ redirect: '/' })
+      localStorage.setItem('ponto', 1)
+    }
     render() {
         return(
-            <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
-              <div className="container">
-                <div className="navbar-header page-scroll">
-                  <a className="navbar-brand page-scroll" href="#page-top">
-                      <img src="/images/logo.png" height="50" />
-                  </a>
-                </div> 
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+              <a className="navbar-brand" href="#"><img src='images/logo.png' width='200' /></a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+            
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">Ponto<span className="sr-only">(current)</span></a>
+                  </li>
+                  {/* <li className="nav-item">
+                    <a className="nav-link" href="#">Link 2</a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a className="dropdown-item" href="#">Action</a>
+                      <a className="dropdown-item" href="#">Another action</a>
+                      <div className="dropdown-divider"></div>
+                      <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link disabled" href="#">Disabled</a>
+                  </li> */}
+                </ul>
+                <form className="form-inline my-2 my-lg-0 form-group" >
+                  <label>Matrícula:</label>
+                  <input className="form-control mr-sm-2" type="search" placeholder="Matrícula" aria-label="Search"/>
+                  <label>Data:</label>
+                  <input className="form-control mr-sm-2" type="date" placeholder="Search" aria-label="Data" />
+                  <button className="btn btn-outline-success my-2 my-sm-0" onClick={() => this.getPonto()}>Verificar</button>
+                </form>
               </div>
-            </nav> 
+            </nav>
         )
     }
 }
